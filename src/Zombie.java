742b9d699c938f671c59cd19326d3e70975e4130
PieIsSpy <karldeejayomandac@gmail.com>
2025-06-07 13:58:26 +0800

    added skeleton code for Zombie class

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,2 +1,41 @@
+/** The class Zombie represents a basic zombie object
+ *  that can be used by other child classes that represent
+ *  variant zombies.
+ *
+ *  @author Karl Deejay Omandac
+ *  @author Rachel Angeline Alba
+ *  @version 1.0
+ */
 public class Zombie {
+    /** This constructor initializes the default values
+     *  of a basic zombie. This also increments the static variable
+     *  "count" by 1.
+     */
+    public Zombie() {
+        speed = 4;
+        damage = 10;
+        health = 70;
+        count++;
+    }
+
+    public void walk() {
+
+    }
+
+    public void eat() {
+
+    }
+
+    public void die() {
+        count--;
+    }
+
+    /** How fast the zombie moves */
+    private int speed;
+    /** How much damage it deals to the plant it is attacking */
+    private int damage;
+    /** How much damage it can sustain */
+    private int health;
+    /** How many zombies are created */
+    private static int count = 0;
 }
