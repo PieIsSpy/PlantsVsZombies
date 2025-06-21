377a87f144c98c88ede1e52ea78660219f701cb7
PieIsSpy <karldeejayomandac@gmail.com>
2025-06-21 18:44:17 +0800

    removed display methods

--- Level.java
+++ Level.java
@@ -66,22 +66,6 @@ public class Level {
         }
     }
 
-    public void displayChar(int n, char c) {
-        int i;
-        for (i = 0; i < n; i++)
-            System.out.print(c);
-    }
-
-    public void displayLine() {
-        int i, j;
-        displayChar(1, '+');
-
-        for (i = 0; i < COLUMNS; i++) {
-            displayChar(4, '-');
-            displayChar(1, '+');
-        }
-    }
-
     public void displayLawn() {
 
     }
