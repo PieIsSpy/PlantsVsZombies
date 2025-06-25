aa65b2ce195c1ef71b3ca86c2d92eecbb9a45a01
rachell-code <albarachel326@gmail.com>
2025-06-25 12:37:32 +0800

    Add files via upload

---
+++ GameElement.java
@@ -0,0 +1,116 @@
+/** This class is to be inherited by objects that exist in 
+ * the game but do not carry entity related attributes such as
+ * health or attack values. These shared attributes and 
+ * methods are to be used by temporary game elements such
+ * as projectile and sun objects. 
+ *
+ *  @author Karl Deejay Omandac
+ *  @author Rachel Angeline Alba
+ *  @version 1.0
+ */
+
+public class GameElement {
+
+    /**
+     * This constructor initializes the position of 
+     * the game element. It also sets it as an active
+     * objective. 
+     * 
+     * @param r row position
+     * @param c column position
+     */
+    public GameElement(float r, float c)
+    {
+        row = r;
+        col = c;
+        isActive = true;
+    }
+ 
+    /**
+     * This method updates the behavior or actions
+     * of the game element for every given time. 
+     * 
+     */
+    public void update()
+    {
+        
+    }
+    /**
+     * This method deactivates a game element, allowing it to
+     * be removed from the game. 
+     * 
+     */
+    public void deactivate()
+    {
+        isActive = false;
+    }
+
+    /**
+     * 
+     * This method returns the active status of 
+     * a game element. 
+     * 
+     * @return true if game element is still active, 
+     * false otherwise. 
+     */
+    public boolean isActive()
+    {
+        return isActive;
+    }
+
+    /**
+     * This method returns the current row position
+     * of a game element. 
+     * 
+     * @return row position of game element. 
+     */
+    public float getRow()
+    {
+        return row;
+    }
+
+    /**
+     * 
+     * This method returns the current column
+     * position of a game element. 
+     * 
+     * @return column position of projectile
+     */
+    public float getCol()
+    {
+        return col;
+    }
+
+    /**
+     * This method sets the columns position
+     * of a game element object
+     * 
+     * 
+     * @param c column position of projectile
+     */
+    public void setCol(float c)
+    {
+        col = c;
+    }
+
+    /**
+     * This method sets the row position
+     * of a game element object
+     * 
+     * 
+     * @param c row position of game element
+     */
+    public void setRow(float r)
+    {
+        row = r;
+    }
+
+
+    /**row position of game element */
+    private float row;
+    /**column position of game element */
+    private float col;
+    /**active status of game element */
+    private boolean isActive;
+
+}
---
+++ Sun.java
@@ -0,0 +1,119 @@
+
+public class Sun extends GameElement {
+
+    public Sun(float row, float c, boolean fromSky, float targetRow)
+    {
+        super(row, c);
+        this.fromSky = fromSky;
+        this.targetRow = targetRow;
+        landedTime = -888;
+
+    }
+
+    public Sun(float row, float c, boolean fromSky)
+    {
+        super(row, c);
+        targetRow = row; //row: 1, col: 2, targetRow = row
+        this.fromSky = fromSky;
+        landedTime = -888;
+    }
+
+    public void update(int currentTime)
+    {
+        if(fromSky) //if sun is from the sky
+        {
+            fallFromSky();
+        }
+
+        landAtTargetRow(currentTime);
+        disappear(currentTime);
+    }
+
+
+    public void fallFromSky()
+    {
+        
+        float currentRow = getRow();
+
+        if(fromSky && !hasLanded())
+        {
+            currentRow += 1f / FALLING_SPEED;
+            setRow(currentRow);
+            System.out.println("Sun is falling at (" + getRow() + ", " + getCol() + ")");
+        }
+    }
+
+    //true if sun has landed, false if not
+
+    
+    public void landAtTargetRow(int currentTime) //where it will land or its final position
+    {
+        if(hasLanded() && landedTime < 0)
+        {
+            setRow(targetRow);
+            landedTime = currentTime;
+            fromSky = false;
+            System.out.println("Sun is has landed in (" + getRow() + ", " + getCol() + ")");
+        }
+        
+    }
+
+    //after 10 seconds, sun should deactivate/disappear
+
+    public void disappear(int currentTime)
+    {
+
+        //landedTime 
+
+        if(landedTime >= 0)
+        {
+            //if its time for sun to disappear
+            if(currentTime >= (LIFETIME + landedTime))
+            {
+                deactivate();
+                System.out.println("Sun has disappeared!");
+            }
+            else
+            {
+            
+                System.out.println("Sun has " + ((landedTime + LIFETIME) - currentTime) + " remaining seconds beofre it disappears.");
+            }
+
+            
+        }
+        
+    }
+
+    public boolean hasLanded()
+    {
+        return getRow() >= targetRow;
+    }
+
+    public int getAmount()
+    {
+        return AMOUNT;
+    }
+
+   /*  public static void main(String[] args)
+    {
+        Sun sun = new Sun(2, 3, false);
+        int i;
+
+        for(i = 0; i < 20 && sun.isActive(); i++)
+        {
+            sun.update(i);
+
+        }
+    }
+    */
+
+    private final int AMOUNT = 25;
+    private final float FALLING_SPEED = 2f;
+    private final int LIFETIME = 10;
+    private boolean fromSky;
+    private float targetRow;
+    private int landedTime;
+
+
+
+}
