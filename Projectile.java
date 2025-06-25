6225a146280d9de4fd85491f818628f507e9097b
rachell-code <albarachel326@gmail.com>
2025-06-25 12:41:44 +0800

    updated changes form gameElement

--- Projectile.java
+++ Projectile.java
@@ -1,4 +1,5 @@
-/** The class Projectile represents a projectile fired by
+
+/** The class Projectile represents a projectile fired by 
  * a plant object. It is responsible for handling its 
  * movement and dealing damage to a zombie object.  
  *
@@ -7,7 +8,7 @@
  *  @version 1.0
  */
 
-public class Projectile {
+public class Projectile extends GameElement{
 
     /**
      * This constructor initializes the attributes
@@ -20,12 +21,11 @@ public class Projectile {
      * @param dmg damage it deals to another object
      * @param s speed of projectile 
      */
-    Projectile(float r, float c, int dmg, float s)
+    Projectile(float row, float col, int dmg, float s)
     {
-        col = c;
+        super(row, col);
         speed = s;
         damage = dmg;
-        isActive = true;
     }
 
     /**
@@ -37,95 +37,33 @@ public class Projectile {
      * 
      * @param z target Zombie object
      */
-    public void hitEnemy(Zombie z)
+    public void hit(Zombie z)
     {
         z.takeDamage(damage);
-        deactivateProjectile();
+        deactivate();
 
-        System.out.println("Projectile has hit zombie at ( " + z.getRow() + ", " + z.getCol() + ")");
+        System.out.println("Projectile has hit zombie at (" + z.getRow() + ", " + z.getCol() + ")");
     }
 
     /**
-     * This method is responsible for the movement
+     * This method is updates the movement
      * of a projectile after it is released. 
      * 
      */
-    public void moveProjectile()
+   
+    public void update()
     {
-        float pos = getColumn();
+        float pos = getCol();
 
         pos += (float)(1.0 / speed);
-        setColumn(pos);
+        setCol(pos);
+        
     }
 
-    /**
-     * This method deactivates a projectile, allowing it to
-     * be removed from the game. It is usually used when 
-     * it has already hit a zombie object or has gone
-     * out of the game grid. 
-     * 
-     */
-    public void deactivateProjectile()
-    {
-        isActive = false;
-    }
 
-    /**
-     * 
-     * This method returns the active status of 
-     * a projectile. 
-     * 
-     * @return true if projectile is still active, 
-     * false otherwise. 
-     */
-    public boolean isActive()
-    {
-        return isActive;
-    }
-
-    /**
-     * This method sets the columns position
-     * of a projectile object
-     * 
-     * 
-     * @param c column position of projectile
-     */
-    public void setColumn(float c)
-    {
-        col = c;
-    }
-
-    /**
-     * 
-     * This method returns the current column
-     * position of a projectile. 
-     * 
-     * @return column position of projectile
-     */
-    public float getColumn()
-    {
-        return col;
-    }
-
-    /**
-     * This method returns the current row position
-     * of a projectile. 
-     * 
-     * @return row position of projectile
-     */
-    public float getRow()
-    {
-        return row;
-    }
-
-    /**row position of projectile */
-    private float row;
-    /**column position of projectile */
-    private float col;
     /**value of damage dealt by projectile */
     private int damage;
     /**how fast the projectile moves once released */
     private float speed;
-    /**flag to check if projectile is still active or not*/
-    private boolean isActive;
+    
 }
