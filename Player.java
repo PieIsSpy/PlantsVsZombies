085ba95f255dd8afe4f800d0411093f890d08786
PieIsSpy <karldeejayomandac@gmail.com>
2025-06-26 19:53:16 +0800

    documentation

--- Player.java
+++ Player.java
@@ -1,19 +1,39 @@
 /** This class represents the Player, the controller
  *  of the game.
  *
- *
- *
+ *  @author Karl Deejay Omandac
+ *  @author Rachel Angeline Alba
+ *  @version 1.0
  */
 public class Player {
 
+    /** This constructor initializes the initial amount of
+     *  suns the player have.
+     *
+     * @param s the initial amount of suns
+     */
     public Player (int s) {
         sun = s;
     }
 
+    /** This method returns the player's current amount of suns
+     *
+     * @return the player's current amount of suns
+     */
     public int getSun() {
         return sun;
     }
 
+    /** This method instantiates a plant to a Level class
+     *  in a given row and column, provided that the given
+     *  name of the plant is valid.
+     *
+     * @param l the level to instantiate the plant
+     * @param r the row of the plant to be instantiated
+     * @param c the col of the plant to be instantiated
+     * @param n the name of the plant to be instantiated
+     * @param t the time of instantiation
+     */
     public void placePlant(Level l, int r, int c, String n, int t) {
         if (n.equalsIgnoreCase("sunflower"))
             l.getTiles()[r][c] = new Sunflower(r,c,t);
@@ -21,17 +41,36 @@ public class Player {
             l.getTiles()[r][c] = new Peashooter(r,c, t);
     }
 
+    /** This method removes a plant in a given row
+     *  and col of a Level class.
+     *
+     * @param l the level to be edited
+     * @param r the row to be shoveled
+     * @param c the col to be shoveled
+     */
     public void useShovel(Level l, int r, int c) {
         l.getTiles()[r][c] = null;
     }
 
+    /** This method collects the sun present in a level
+     *  and adds its value to the player's accumulated
+     *  amount of suns.
+     *
+     * @param s the amount of suns to be added
+     */
     public void collectSun(int s) {
         sun += s;
     }
 
+    /** This method subtracts the player's
+     *  accumulated amount of suns.
+     *
+     * @param s the amount of suns to be subtracted
+     */
     public void subtractSun(int s) {
         sun -= s;
     }
 
+    /**the accumulated amount of suns*/
     private int sun;
 }
--- Sun.java
+++ Sun.java
@@ -1,23 +1,50 @@
-
+/** This class represents a sun, the currency of the player
+ *  that allows them to plant seeds in their lawn.
+ *
+ *  @author Karl Deejay Omandac
+ *  @author Rachel Angeline Alba
+ *  @version 1.0
+ */
 public class Sun extends GameElement {
-
-    public Sun(float row, float c, boolean fromSky, float targetRow, int time)
+    /** This constructor instantiate its initial row and col positions,
+     *  the target row to land on and its creation time. This also
+     *  determines if it came from the sky or not.
+     *
+     * @param row the row coordinate of the sun
+     * @param col the col coordinate of the sun
+     * @param fromSky determines if it came from the sky
+     * @param targetRow the target row to land on
+     * @param time time of creation
+     */
+    public Sun(float row, float col, boolean fromSky, float targetRow, int time)
     {
-        super(row, c, time);
+        super(row, col, time);
         this.fromSky = fromSky;
         this.targetRow = targetRow;
         landedTime = -888;
-
     }
 
-    public Sun(float row, float c, boolean fromSky, int time)
+    /** This constructor instantiate its initial row and col positions,
+     *  and its time of creation. It also determines if it came from the sky.
+     *
+     * @param row the row coordinate of the sun
+     * @param col the col coordinate of the sun
+     * @param fromSky determines if it came from the sky
+     * @param time time of creation
+     */
+    public Sun(float row, float col, boolean fromSky, int time)
     {
-        super(row, c, time);
+        super(row, col, time);
         targetRow = row; //row: 1, col: 2, targetRow = row
         this.fromSky = fromSky;
         landedTime = -888;
     }
 
+    /** This is essentially the behavior method of the sun.
+     *  It updates its current position and state.
+     *
+     * @param currentTime the current time reference of the game
+     */
     public void update(int currentTime)
     {
         if(fromSky) //if sun is from the sky
@@ -29,7 +56,10 @@ public class Sun extends GameElement {
         disappear(currentTime);
     }
 
-
+    /** This updates the position of the sun going down.
+     *
+     * @param currentTime the current time reference of the game
+     */
     public void fallFromSky(int currentTime)
     {
         
@@ -43,9 +73,12 @@ public class Sun extends GameElement {
         }
     }
 
-    //true if sun has landed, false if not
-
-    
+    /** This initializes the position state of the sun if it has
+     *  landed or not. This also initializes the landTime of the sun.
+     *  and changes it's row to the targetRow.
+     *
+     * @param currentTime the current time reference of the game
+     */
     public void landAtTargetRow(int currentTime) //where it will land or its final position
     {
         if(hasLanded() && landedTime < 0)
@@ -55,19 +88,19 @@ public class Sun extends GameElement {
             fromSky = false;
             System.out.println("Sun is has landed in (" + (getRow()+1) + ", " + (getCol()+1) + ")");
         }
-        
     }
 
-    //after 10 seconds, sun should deactivate/disappear
-
+    /** This method deactivates the active state of the sun object
+     *  after 10 seconds when landed.
+     *
+     * @param currentTime the current time reference of the game
+     */
     public void disappear(int currentTime)
     {
-
-        //landedTime 
-
+        //after 10 seconds, sun should deactivate/disappear
         if(landedTime >= 0)
         {
-            //if its time for sun to disappear
+            //if it's time for sun to disappear
             if(currentTime >= (LIFETIME + landedTime))
             {
                 deactivate();
@@ -75,45 +108,39 @@ public class Sun extends GameElement {
             }
             else
             {
-            
                 System.out.println("Sun has " + ((landedTime + LIFETIME) - currentTime) + " remaining seconds before it disappears.");
             }
-
-            
         }
-        
     }
 
+    /** This method determines if the sun object have landed or not.
+     *
+     * @return true if the sun object have landed, else false
+     */
     public boolean hasLanded()
     {
         return getRow() >= targetRow;
     }
 
+    /** This method returns the value of the sun object.
+     *
+     * @return the value of the sun object
+     */
     public int getAmount()
     {
         return AMOUNT;
     }
 
-   /*  public static void main(String[] args)
-    {
-        Sun sun = new Sun(2, 3, false);
-        int i;
-
-        for(i = 0; i < 20 && sun.isActive(); i++)
-        {
-            sun.update(i);
-
-        }
-    }
-    */
-
+    /**the value of the sun*/
     private final int AMOUNT = 25;
+    /**the speed of the sun falling down*/
     private final float FALLING_SPEED = 2f;
+    /**the uptime of the sun object*/
     private final int LIFETIME = 10;
+    /**determines if it came from the sky*/
     private boolean fromSky;
+    /**the target row of the sun when falling down*/
     private float targetRow;
+    /**the time when the sun landed*/
     private int landedTime;
-
-
-
 }
