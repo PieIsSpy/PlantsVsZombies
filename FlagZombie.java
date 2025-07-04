527f7cdb47e31d527f8084bc99ab1b72d83f926b
PieIsSpy <karldeejayomandac@gmail.com>
2025-07-04 10:29:52 +0800

    awsome inheritance bro

---
+++ FlagZombie.java
@@ -0,0 +1,7 @@
+public class FlagZombie extends Zombie{
+    public FlagZombie (int r, int c, int t) {
+        super(r, c, t);
+        setSpeed(3);
+        setHeld_item(new Item("Flag"));
+    }
+}
