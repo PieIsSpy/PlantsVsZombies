63a28009d467316002b482f0e8ee7c572e78edf5
PieIsSpy <karldeejayomandac@gmail.com>
2025-07-04 20:26:18 +0800

    finished implementation of snow pea and slowness projectile

---
+++ SlownessProjectile.java
@@ -0,0 +1,41 @@
+/** The class SlownessProjectile represents a projectile fired by
+ * a snow pea object. It is responsible for handling its
+ * movement and dealing damage and slowness debuff to a zombie object.
+ *
+ *  @author Karl Deejay Omandac
+ *  @author Rachel Angeline Alba
+ *  @version 1.0
+ */
+public class SlownessProjectile extends Projectile{
+    /** This constructor initializes the attributes of a slowness projectile
+     * such as its positions, damage and speed. It also sets the slowness projectile
+     * active.
+     *
+     * @param row the row position of the slowness projectile
+     * @param col the col position of the slowness projectile
+     * @param time the time of creation of the slowness projectile
+     * @param dmg the damage it deals to zombie objects
+     * @param s the speed of the slowless projectile
+     */
+    public SlownessProjectile(float row, float col, int time, int dmg, float s) {
+        super(row,col,time,dmg,s);
+    }
+
+    /**
+     * This method is responsible for dealing damage
+     * to its targeted zombie object. Once it hits its
+     * target, it gives the zombie with a slowness debuff and
+     * deactivates the projectile, allowing it
+     * to be removed.
+     *
+     *
+     * @param z target Zombie object
+     */
+    @Override
+    public void hit(Zombie z)
+    {
+        z.takeDamage(getDamage());
+        z.setSlowed(true);
+        deactivate();
+    }
+}
---
+++ SnowPea.java
@@ -0,0 +1,48 @@
+/** This class represents the Snow Pea plant, which is a child class
+ *  of Peashooter class. This defines how snow pea plants behave towards
+ *  zombie enemies and slows them down when its projectiles hit them.
+ *
+ *  @author Karl Deejay Omandac
+ *  @author Rachel Angeline Alba
+ *  @version 1.0
+ */
+public class SnowPea extends Peashooter{
+    /** This constructor initializes the attributes of a snow pea
+     *  plant and places it into a given row and col position.
+     *  This also initializes the time of its creation.
+     *
+     * @param r the row coordinates of the plant
+     * @param c the col coordinates of the plant
+     * @param currentTime the time of its creation
+     */
+    public SnowPea(int r, int c, int currentTime) {
+        super(r,c,currentTime);
+        setCost(175);
+    }
+
+    /**
+     *
+     * This method represents the shooting of a projectile
+     * at its targeted zombie object. It adds it to the
+     * peashooter's list of projectiles. If the target is within
+     * a direct damage range, the projectile will deal an increased
+     * amount of damage. The current time reference
+     *
+     * @param z the zombie to be targeted
+     * @param currentTime the current time reference of the game
+     * @param level the level to add peas projectile
+     */
+    @Override
+    public void shoot(Zombie z, int currentTime, Level level)
+    {
+        //System.out.println("Pew!");
+        if(isWithinDirectDamage(z.getCol()))
+        {
+            level.getPeas().add(new SlownessProjectile(getRow(), getCol(), currentTime, getDirectDamage(), 1.5f));
+        }
+        else if (isWithinRange(z.getCol()))
+        {
+            level.getPeas().add(new SlownessProjectile(getRow(), getCol(), currentTime, getDamage(), 1.5f));
+        }
+    }
+}
