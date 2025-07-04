2854021e3483f0dd3173a69bd39f35265eb88930
rachell <albarachel326@gmail.com>
2025-07-04 12:49:00 +0800

    added background image in gui

--- ProgramGUI.java
+++ ProgramGUI.java
@@ -3,11 +3,27 @@ import java.awt.*;
 
 public class ProgramGUI extends JFrame {
     public ProgramGUI() {
+
         super("Plants vs Zombies");
-        setLayout(new BorderLayout());
-        setSize(900, 650);
+        setLayout(null);
+        setSize(1052, 594);
+        init();
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    } 
+
+
+    public void init()
+    {
+        //BACKGROUND IMAGE
+        background = new JLabel(new ImageIcon("lawnImage.png"), JLabel.CENTER);
+        background.setBounds(0, 0, 1052, 594);
+        add(background);
+
     }
+
+    private Image lawn;
+    private JLabel background;
+    //use grid layout for the center 
 }
