c972e9f01fc11a6a1ed380b8eb30062cc3e5b88f
PieIsSpy <karldeejayomandac@gmail.com>
2025-07-04 10:45:17 +0800

    initialized the item into the 2nd super constructor instead of using setHeld_item()

--- FlagZombie.java
+++ FlagZombie.java
@@ -1,7 +1,6 @@
 public class FlagZombie extends Zombie{
     public FlagZombie (int r, int c, int t) {
-        super(r, c, t);
+        super(r, c, t, new Item("Flag"));
         setSpeed(3);
-        setHeld_item(new Item("Flag"));
     }
 }
