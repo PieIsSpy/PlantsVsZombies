60867c3b5223a4ffbca790c5fd171d7a8b5cd43b
PieIsSpy <karldeejayomandac@gmail.com>
2025-07-07 19:36:31 +0800

    commented model for now

--- PlantsVsZombies.java
+++ PlantsVsZombies.java
@@ -1,6 +1,6 @@
 public class PlantsVsZombies {
     public static void main(String[] args) {
-        Level level1 = new Level(1,180,5,9,0);
+        //Level level1 = new Level(1,180,5,9,0);
         ProgramGUI view = new ProgramGUI();
         Controller controller = new Controller();
     }
