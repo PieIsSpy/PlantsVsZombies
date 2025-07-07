95c012b702579fa23130d0a2fb5a2885f792b593
rachell <albarachel326@gmail.com>
2025-07-07 15:41:56 +0800

    updated (PLEASE WORK HUHU)

--- LawnPanel.java
+++ LawnPanel.java
@@ -1,18 +1,17 @@
 import java.awt.Graphics;
-import java.io.File;
-
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class LawnPanel extends JPanel {
     public LawnPanel()
     {
-        File file = new File("lawnImage.png");
+        /*File file = new File("lawnImage.png");
         System.out.println("can execute: " + file.canExecute());
         System.out.println("location: " + file.getAbsolutePath());
         System.out.println("does file exist: " + file.exists());
+        */ 
 
-        lawnImg = new ImageIcon("lawnImage.png");
+        lawnImg = new ImageIcon(getClass().getResource("/lawnImage.png"));
         System.out.println("image width: " + lawnImg.getImage().getWidth(null));
         System.out.println("image height: " + lawnImg.getImage().getHeight(null));
 
