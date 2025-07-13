11db04a13679bdd3edaf51d9d9a63b1e25f43cbe
PieIsSpy <karldeejayomandac@gmail.com>
2025-07-14 07:25:29 +0800

    trying to implement threading in mvc

--- Controller.java
+++ Controller.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -5,6 +6,7 @@ public class Controller implements ActionListener {
     public Controller(MainModel m, ProgramGUI v) {
         model = m;
         view = v;
+        view.setActionListener(this);
     }
 
     public void updateView() {
@@ -13,7 +15,41 @@ public class Controller implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // menu
+        if (e.getActionCommand().equals("Start")) {
+            System.out.println("Pressed start");
+            view.levelSelect();
+        }
+        else if (e.getActionCommand().equals("Quit")) {
+            System.out.println("Pressed quit");
+            view.dispose();
+        }
 
+        // level selector
+        else if(e.getActionCommand().equals("Level 1")) {
+            if (!model.isInProgress()) {
+                System.out.println("Pressed Level 1");
+                model.selectLevel(1);
+            }
+            else
+                System.out.println("A level is already running!");
+        }
+        else if (e.getActionCommand().equals("Level 2")) {
+            if (!model.isInProgress()) {
+                System.out.println("Pressed Level 2");
+                model.selectLevel(2);
+            }
+            else
+                System.out.println("A level is already running!");
+        }
+        else if (e.getActionCommand().equals("Level 3")) {
+            if (!model.isInProgress()) {
+                System.out.println("Pressed Level 3");
+                model.selectLevel(3);
+            }
+            else
+                System.out.println("A level is already running!");
+        }
     }
 
     private MainModel model;
--- MainModel.java
+++ MainModel.java
@@ -1,8 +1,14 @@
-public class MainModel {
+public class MainModel extends Thread {
+    public MainModel() {
+        inProgress = false;
+    }
+
     public void selectLevel(int levelNum) {
+        System.out.println("Called selectLevel()");
         if (levelNum == 1 && levelProgress >= 0) {
             Level1 level1 = new Level1((int)System.currentTimeMillis()/1000);
-            levelDriver(level1);
+            //new Thread ()
+
             level1 = null;
         }
         else if (levelNum == 2 && levelProgress >= 1) {
@@ -15,17 +21,39 @@ public class MainModel {
             levelDriver(level3);
             level3 = null;
         }
+        else
+            System.out.println("Level not yet unlocked");
     }
 
     public void levelDriver(Level level) {
-        while (!level.isGameOver() && level.isGameWon((int)System.currentTimeMillis()/1000)) {
-            level.gameCycle((int)System.currentTimeMillis()/1000);
-        }
+        int levelStart, levelTimer, lastPrint = -1;
+        System.out.println("Level running");
+        inProgress = true;
+
+        levelStart = (int)System.currentTimeMillis()/1000;
+        do {
+            levelTimer = (int)System.currentTimeMillis()/1000 - levelStart;
+
+            if (System.currentTimeMillis() % 1000 == 0 && levelTimer != lastPrint) {
+                System.out.println("Level 1 running: " + (levelTimer - levelProgress));
+                lastPrint = levelTimer;
+            }
+
+            level.gameCycle(levelTimer);
+        } while (!level.isGameOver() && !level.isGameWon(levelTimer));
+        System.out.println("done");
+
+        inProgress = false;
 
         if (level.isGameWon((int)System.currentTimeMillis()/1000))
             levelProgress++;
     }
 
+    public boolean isInProgress() {
+        return inProgress;
+    }
+
+    private boolean inProgress;
     private int levelProgress = 0;
 }
 
